5cc85c3fe5973b85d1d0eb0192593e72e2097b52
Dominik <dodosmon@gmial.com>
2020-07-05 21:42:38 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='bs4_package',
+      version='1.0',
+      description='Getting hrefs, text and html code from any page',
+      author='Dominik Semsch',
+      author_email='dominik.semsch@gmail.com',
+      url='https://www.python.org/sigs/distutils-sig/',
+      packages=["get_all_href's", "get_all_p_paragraphs", "insert_webpage"],
+     )
